1beebf44bb58fe116c097bbb85f979ac77caae3a
Jamie Weavis <jamie.weavis@gmail.com>
2017-05-30 13:21:07 +0100

    :hammer: Refactor `index.js` to be cleaner and more concise

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const pjson = require('./package.json')
 const config = require('./app/config')
 const applescript = require('applescript')
 const AutoLaunch = require('auto-launch')
@@ -7,115 +8,122 @@ const AutoLaunch = require('auto-launch')
 const electron = require('electron')
 const { app, globalShortcut, Tray, Menu, BrowserWindow, shell, ipcMain } = electron
 
-const autoLaunch = new AutoLaunch({ name: app.getName(), path: `/Applications/${app.getName()}.app` })
-let tray = null
-let preferencesWindow = null
-let aboutWindow = null
-
-app.dock.hide()
 app.on('ready', () => {
-  tray = new Tray(`${__dirname}/app/iconTemplate.png`)
-  tray.on('click', onTrayClick)
-  tray.on('right-click', onTrayRightClick)
+  const tray = new Tray(`${__dirname}/app/iconTemplate.png`)
+  const afkAutoLauncher = new AutoLaunch({
+    name: app.getName(),
+    path: `/Applications/${pjson.name}.app`
+  })
+
+  let preferencesWindow = null
+  let aboutWindow = null
+
+  function createTrayMenu () {
+    let cfg = config.store
+    let menuTemplate = [
+      { label: `Start ${pjson.name}`, accelerator: cfg.globalHotkey, click: onActivate },
+      {
+        label: 'Mode',
+        submenu: [{
+          label: 'Screensaver',
+          type: 'radio',
+          checked: cfg.mode === 'Screensaver',
+          click: onModeChange
+        }, {
+          label: 'Sleep',
+          type: 'radio',
+          checked: cfg.mode === 'Sleep',
+          click: onModeChange
+        }, {
+          label: 'Lock',
+          type: 'radio',
+          checked: cfg.mode === 'Lock',
+          click: onModeChange
+        }]
+      },
+      { label: 'Preferences', accelerator: 'Cmd+,', click: createPreferencesWindow },
+      { type: 'separator' },
+      { label: `About ${pjson.name}`, click: createAboutWindow },
+      { label: 'Feedback && Support...', click: () => { shell.openExternal(pjson.bugs.url) } },
+      { type: 'separator' },
+      { label: `Quit ${pjson.name}`, accelerator: 'Cmd+Q', click: () => { app.quit() } }
+    ]
+    return Menu.buildFromTemplate(menuTemplate)
+  }
+
+  function createPreferencesWindow () {
+    if (preferencesWindow) return preferencesWindow.focus()
+    preferencesWindow = new BrowserWindow({
+      title: `${app.getName()} Preferences`,
+      titleBarStyle: 'hidden-inset',
+      width: 350,
+      height: 260,
+      resizable: false,
+      maximizable: false,
+      show: false
+    })
+    preferencesWindow.loadURL(`file://${__dirname}/app/preferences.html`)
+    preferencesWindow.once('ready-to-show', () => {
+      preferencesWindow.show()
+      app.dock.show()
+    })
+    preferencesWindow.on('closed', () => {
+      preferencesWindow = null
+      app.dock.hide()
+    })
+  }
+
+  function createAboutWindow () {
+    if (aboutWindow) return aboutWindow.focus()
+    aboutWindow = new BrowserWindow({
+      title: `About ${app.getName()}`,
+      titleBarStyle: 'hidden-inset',
+      width: 350,
+      height: 220,
+      resizable: false,
+      maximizable: false,
+      show: false
+    })
+    aboutWindow.loadURL(`file://${__dirname}/app/about.html`)
+    aboutWindow.once('ready-to-show', () => {
+      aboutWindow.show()
+      app.dock.show()
+    })
+    aboutWindow.on('closed', () => {
+      aboutWindow = null
+      app.dock.hide()
+    })
+  }
+
+  function onModeChange (radio) {
+    config.set('mode', radio.label)
+    if (preferencesWindow) preferencesWindow.reload()
+  }
+
+  function onActivate () {
+    let mode = config.get('mode')
+    let unpackedPath = __dirname.replace('app.asar', 'app.asar.unpacked')
+    tray.setHighlightMode('always')
+
+    if (mode === 'screensaver') {
+      let delay = config.get('delay')
+      setTimeout(() => {
+        applescript.execFile(`${unpackedPath}/app/applescript/screensaver.applescript`)
+        tray.setHighlightMode('selection')
+      }, delay)
+    } else {
+      applescript.execFile(`${unpackedPath}/app/applescript/${mode}.applescript`)
+      setTimeout(() => { tray.setHighlightMode('selection') }, 250)
+    }
+  }
+
+  app.dock.hide()
+  app.on('window-all-closed', () => {})
+
+  tray.on('click', () => { config.get('invertClicks') ? onActivate() : tray.popUpContextMenu(createTrayMenu()) })
+  tray.on('right-click', () => { config.get('invertClicks') ? tray.popUpContextMenu(createTrayMenu()) : onActivate() })
 
   globalShortcut.register(config.get('globalHotkey'), onActivate)
-  ipcMain.on('toggleAutoLaunch', setAutoLaunch)
+
+  ipcMain.on('toggleAutoLaunch', (event, checked) => { checked ? afkAutoLauncher.enable() : afkAutoLauncher.disable() })
 })
-app.on('window-all-closed', () => {})
-
-function createTrayMenu () {
-  return Menu.buildFromTemplate([
-    { label: `Start ${app.getName()}`, accelerator: config.get('globalHotkey'), click: onActivate },
-    {
-      label: 'Mode',
-      submenu: Menu.buildFromTemplate([
-        { label: 'Screensaver', value: 'screensaver', type: 'radio', checked: config.get('mode') === 'screensaver', click: onModeChange },
-        { label: 'Sleep', value: 'sleep', type: 'radio', checked: config.get('mode') === 'sleep', click: onModeChange },
-        { label: 'Lock', value: 'lock', type: 'radio', checked: config.get('mode') === 'lock', click: onModeChange }
-      ])
-    },
-    { label: 'Preferences', accelerator: 'Cmd+,', click: createPreferencesWindow },
-    { type: 'separator' },
-    { label: `About ${app.getName()}`, click: createAboutWindow },
-    { label: 'Feedback && Support...', click: () => { shell.openExternal('https://github.com/jamiestraw/afk/issues') } },
-    { type: 'separator' },
-    { label: `Quit ${app.getName()}`, accelerator: 'Cmd+Q', click: () => { app.quit() } }
-  ])
-}
-
-function createPreferencesWindow () {
-  if (preferencesWindow) return preferencesWindow.focus()
-  preferencesWindow = new BrowserWindow({
-    title: `${app.getName()} Preferences`,
-    titleBarStyle: 'hidden-inset',
-    width: 350,
-    height: 260,
-    resizable: false,
-    maximizable: false,
-    show: false
-  })
-  preferencesWindow.loadURL(`file://${__dirname}/app/preferences.html`)
-  preferencesWindow.once('ready-to-show', () => {
-    preferencesWindow.show()
-    app.dock.show()
-  })
-  preferencesWindow.on('closed', () => {
-    preferencesWindow = null
-    app.dock.hide()
-  })
-}
-
-function createAboutWindow () {
-  if (aboutWindow) return aboutWindow.focus()
-  aboutWindow = new BrowserWindow({
-    title: `About ${app.getName()}`,
-    titleBarStyle: 'hidden-inset',
-    width: 350,
-    height: 220,
-    resizable: false,
-    maximizable: false,
-    show: false
-  })
-  aboutWindow.loadURL(`file://${__dirname}/app/about.html`)
-  aboutWindow.once('ready-to-show', () => {
-    aboutWindow.show()
-    app.dock.show()
-  })
-  aboutWindow.on('closed', () => {
-    aboutWindow = null
-    app.dock.hide()
-  })
-}
-
-function onTrayClick () {
-  config.get('invertClicks') ? onActivate() : tray.popUpContextMenu(createTrayMenu())
-}
-
-function onTrayRightClick () {
-  config.get('invertClicks') ? tray.popUpContextMenu(createTrayMenu()) : onActivate()
-}
-
-function setAutoLaunch (event, value) {
-  value === true ? autoLaunch.enable() : autoLaunch.disable()
-}
-
-function onModeChange (radio) {
-  config.set('mode', radio.value)
-  if (preferencesWindow) preferencesWindow.reload()
-}
-
-function onActivate () {
-  let mode = config.get('mode')
-  let unpackedPath = __dirname.replace('app.asar', 'app.asar.unpacked')
-  tray.setHighlightMode('always')
-
-  if (mode === 'screensaver') {
-    setTimeout(() => {
-      applescript.execFile(`${unpackedPath}/app/applescript/screensaver.applescript`)
-      tray.setHighlightMode('selection')
-    }, config.get('delay'))
-  } else {
-    applescript.execFile(`${unpackedPath}/app/applescript/${mode}.applescript`)
-    setTimeout(() => { tray.setHighlightMode('selection') }, 250)
-  }
-}
